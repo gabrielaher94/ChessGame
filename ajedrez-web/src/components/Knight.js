3dbbbce6e8a5bd078afdd46f2e0ce6a99662a384
DESKTOP-F47U60N\gabri <gabrielahercules94@gmail.com>
2025-11-10 21:45:08 -0600

    Codigo modificado

--- ajedrez-web/src/components/Knight.js
+++ ajedrez-web/src/components/Knight.js
@@ -2,19 +2,25 @@
     import Piece from "./Piece";
 
     class Knight extends Piece {
-    getValidMoves(board) {
+    getValidMoves(board, position) {
         const moves = [];
-        const [row, col] = this.position;
+        const [row, col] = position;
+
         const jumps = [
-        [-2, -1], [-2, 1], [2, -1], [2, 1],
-        [-1, -2], [-1, 2], [1, -2], [1, 2],
+        [-2, -1], [-2, 1],
+        [-1, -2], [-1, 2],
+        [1, -2], [1, 2],
+        [2, -1], [2, 1],
         ];
 
         for (let [dr, dc] of jumps) {
         const r = row + dr;
         const c = col + dc;
+
+        // Verificar que esté dentro del tablero
         if (r >= 0 && r < 8 && c >= 0 && c < 8) {
             const target = board[r][c];
+            // Casilla vacía o con pieza del oponente
             if (!target || target.color !== this.color) {
             moves.push([r, c]);
             }
